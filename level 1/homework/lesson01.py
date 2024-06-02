28d300f1bec6092a6dda00f341bbd923dabb7617
luka3033 <lukachinchaladze3@gmail.com>
2024-06-02 20:08:05 +0400

    first commit

---
+++ level 1/homework/lesson01.py
@@ -0,0 +1,4 @@
+name = "baia"
+name1 = "rubeni"
+print("dedachemis" + " saxelia " + name )
+print("mamachemis" + " saxelia " + name1)
---
+++ level 5/homework/lesson 05.py
@@ -0,0 +1,133 @@
+price = 150 + 50
+print(price)
+
+
+score = 5 + 8
+print(score)
+
+price = 50 + 12
+print(price)
+
+
+price = 20 + 21
+print(price)
+
+price = 20 + 30
+print(price)
+
+price = 34 + 91
+print(price)
+
+price = 57 + 5
+print(price)
+
+price = 40 + 60
+print(price)
+
+price = 91 + 21
+print(price)
+
+price = 41 + 41
+print(price)
+
+
+
+
+price = 7 * 5
+print(price)
+
+price = 3 * 71
+print(price)
+
+price = 5 * 4
+print(price)
+
+price = 9 * 7
+print(price)
+
+price = 7 * 4
+print(price)
+
+price = 5 * 70
+print(price)
+
+price = 21 * 5
+print(price)
+
+price = 24 * 12
+print(price)
+
+price = 2 * 9
+print(price)
+
+price = 64 * 3
+print(price)
+
+
+
+
+price = 5 / 5
+print(price)
+
+price = 125 / 5
+print(price)
+
+price = 61 / 2
+print(price)
+
+price = 675 / 5
+print(price)
+
+price = 5 / 2
+print(price)
+
+price = 10 / 5
+print(price)
+
+price = 16 / 4
+print(price)
+
+price = 64 / 16
+print(price)
+
+price = 102 / 51
+print(price)
+
+price = 64 / 4
+print(price)
+
+price = 126 / 6
+print(price)
+
+
+
+
+price = 25 - 5
+print(price)
+
+price = 143 - 113
+print(price)
+
+price = 21 - 13
+print(price)
+
+price = 21 - 10
+print(price)
+
+price = 1260 - 30
+print(price)
+
+price = 140 - 70
+print(price)
+
+price = 180 - 18
+print(price)
+
+price = 45 - 15
+print(price)
+
+price = 90 - 45
+print(price)
+
+price = 180 - 72
+print(price)
