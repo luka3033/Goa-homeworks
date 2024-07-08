2148d86c3a69540e9b0e9a65a8799f5f68e4d4a4
luka3033 <lukachinchaladze3@gmail.com>
2024-07-08 19:33:44 +0400

    second commit

---
+++ level 17/homework/level 17(2).py
@@ -0,0 +1,7 @@
+
+math = int(input('enter math score'))
+english = int(input('enter english score'))
+russia = int(input('enter russia score'))
+science = int(input('enter science score'))
+music = int(input('enter music score'))
+print((math + english + russia + science + music) // 5)
